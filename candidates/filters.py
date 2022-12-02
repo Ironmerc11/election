5d4c9f1a542ea6b03101a6fc7b113888e8cdef9b
Ironmerc <onasanyatunde1234@gmail.com>
2022-12-03 00:37:41 +0100

    Check to see if ids look up work

--- candidates/filters.py
+++ candidates/filters.py
@@ -1,4 +1,4 @@
-from .models import Candidate
+from .models import Candidate, Location
 from django_filters import rest_framework as filters
 from django.contrib.postgres.search import SearchQuery
 from django.db import models
@@ -13,19 +13,9 @@ class CandidateFilter(filters.FilterSet):
     
     name = filters.CharFilter(field_name='name', lookup_expr='icontains', method='filter_name', distinct=True)
     state = filters.CharFilter(field_name='location__state',lookup_expr='icontains', distinct=True)
-    state_code = filters.CharFilter(field_name='location__state_code', lookup_expr='icontains')
-    senatorial_district = filters.CharFilter(field_name='location__senatorial_district', lookup_expr='icontains')
-    senatorial_district_code = filters.CharFilter(field_name='location__senatorial_district_code',lookup_expr='icontains')
-    federal_constituency = filters.CharFilter(field_name='location__federal_constituency', lookup_expr='icontains')
-    federal_constituency_code = filters.CharFilter(field_name='location__federal_constituency_code',lookup_expr='icontains')
-    state_constituency = filters.CharFilter(field_name='location__state_constituency',lookup_expr='icontains')
-    state_constituency_code = filters.CharFilter(field_name='location__state_constituency_code',lookup_expr='icontains')
     lga = filters.CharFilter(field_name='location__lga',lookup_expr='icontains')
-    lga_code = filters.CharFilter(field_name='location__lga_code',lookup_expr='icontains')
     ward = filters.CharFilter(field_name='location__ward',lookup_expr='icontains')
-    ward_code = filters.CharFilter(field_name='location__ward_code',lookup_expr='icontains')
     polling_unit = filters.CharFilter(field_name='location__polling_unit',lookup_expr='icontains')
-    polling_unit_code = filters.CharFilter(field_name='location__polling_unit_code',lookup_expr='icontains')
     party = filters.CharFilter(field_name='party__name',lookup_expr='icontains')
     year = filters.CharFilter(field_name='position__year',lookup_expr='icontains')
     position= filters.CharFilter(field_name='position__position__name',lookup_expr='icontains')
@@ -44,8 +34,17 @@ class CandidateFilter(filters.FilterSet):
         This method should be overridden if additional filtering needs to be
         applied to the queryset before it is cached.
         """
-        for name, value in self.form.cleaned_data.items():
+        clean_dict = {k: v for k, v in self.form.cleaned_data.items() if v}
+        for name, value in clean_dict.items():
+            print(value)
+            # lookup = "%s__%s" % (self.field_name, self.lookup_expr)
+            # qs = self.get_method(qs)(**{lookup: value})
+            if name == 'lga':
+                lga_id = Location.objects.filter(lga=value).values_list('id', flat=True)
+                print(lga_id)
+                return Candidate.objects.filter(location__id__in=list(lga_id))
             queryset = self.filters[name].filter(queryset, value).distinct().prefetch_related('location').prefetch_related('position')
+            print(queryset)
             assert isinstance(
                 queryset, models.QuerySet
             ), "Expected '%s.%s' to return a QuerySet, but got a %s instead." % (
@@ -56,16 +55,14 @@ class CandidateFilter(filters.FilterSet):
         return queryset
     
 
-    class Meta:
-        model = Candidate
-        # fields = [ 'name',  'min_age', 'max_age', 'gender', 'location']
-        fields = ['name', 'min_age', 'max_age' ,'gender', 'qualifications', 'location', 'party', 'state', 'senatorial_district', 'federal_constituency', 
-                  'state_constituency','lga','ward','polling_unit','position','year','state_code','senatorial_district_code','federal_constituency_code',
-                  'state_constituency_code','lga_code','ward_code','polling_unit_code']
-        
     def filter_name(self, queryset, name, value):
         splitted_name = value.split()
         result = queryset
         for m in range(0, len(splitted_name)):
             result = result.filter(name__icontains=splitted_name[m])
         return result
+    class Meta:
+        model = Candidate
+        # fields = [ 'name',  'min_age', 'max_age', 'gender', 'location']
+        fields = ['name', 'min_age', 'max_age' ,'gender', 'qualifications', 'party', 'state', 'lga','ward','polling_unit','position','year']
+        
