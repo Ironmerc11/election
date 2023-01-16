b45a8a3111e306ad57c2aff77ab76d7c532f4308
Ironmerc <onasanyatunde1234@gmail.com>
2023-01-17 00:15:19 +0100

    Sort fields

--- candidates/filter_select_fields.py
+++ candidates/filter_select_fields.py
@@ -4,40 +4,40 @@ from .models import Candidate, Location, Position
 def get_filter_data(filter, state, senatorial_district, federal_constituency,
                     state_constituency, lga, ward, polling_unit):
     if filter.lower() == 'year':
-        return list(set(Location.objects.all().values_list('year', flat=True))).sort()
+        return sorted(set(Location.objects.all().values_list('year', flat=True)))
     if filter.lower() == 'position':
-        return list(set(Position.objects.all().values_list('name', flat=True))).sort()
+        return sorted(set(Position.objects.all().values_list('name', flat=True)))
     if filter.lower() == 'party':
-        return list(set(Candidate.objects.all().values_list('party', flat=True))).sort()
+        return sorted(set(Candidate.objects.all().values_list('party', flat=True)))
     if filter.lower() == 'qualification':
-        return list(set(Candidate.objects.all().values_list('qualifications', flat=True))).sort()
+        return sorted(set(Candidate.objects.all().values_list('qualifications', flat=True)))
     if filter.lower() == 'state':
-        return list(set(Location.objects.all().values_list('state', flat=True))).sort()
+        return sorted((set(Location.objects.all().values_list('state', flat=True))))
     if filter.lower() == 'senatorial_district':
-        return list(set(Location.objects.filter(state=state).values_list('senatorial_district', flat=True))).sort()
+        return sorted(set(Location.objects.filter(state=state).values_list('senatorial_district', flat=True)))
     if filter.lower() == 'federal_constituency':
         if state:
-            return list(set(Location.objects.filter(state=state).values_list('federal_constituency', flat=True))).sort()
+            return sorted(set(Location.objects.filter(state=state).values_list('federal_constituency', flat=True)))
 
-        return list(set(
+        return sorted(set(
             Location.objects.filter(senatorial_district=senatorial_district).values_list('federal_constituency',
-                                                                                         flat=True))).sort()
+                                                                                         flat=True)))
     if filter.lower() == 'state_constituency':
         if state:
-            return list(set(Location.objects.filter(state=state).values_list('state_constituency', flat=True))).sort()
-        return list(set(
+            return sorted(set(Location.objects.filter(state=state).values_list('state_constituency', flat=True)))
+        return sorted(set(
             Location.objects.filter(federal_constituency=federal_constituency).values_list('state_constituency',
-                                                                                           flat=True))).sort()
+                                                                                           flat=True)))
     if filter.lower() == 'lga':
         if state:
-            return list(set(Location.objects.filter(state=state).values_list('lga', flat=True))).sort()
-        return list(
-            set(Location.objects.filter(state_constituency=state_constituency).values_list('lga', flat=True))).sort()
+            return sorted(set(Location.objects.filter(state=state).values_list('lga', flat=True)))
+        return sorted(
+            set(Location.objects.filter(state_constituency=state_constituency).values_list('lga', flat=True)))
     if filter.lower() == 'ward':
         if state:
-            return list(set(Location.objects.filter(state=state).values_list('ward', flat=True))).sort()
-        return list(set(Location.objects.filter(lga=lga).values_list('ward', flat=True))).sort()
+            return sorted(set(Location.objects.filter(state=state).values_list('ward', flat=True)))
+        return sorted(set(Location.objects.filter(lga=lga).values_list('ward', flat=True)))
     if filter.lower() == 'polling_unit':
         if state:
-            return list(set(Location.objects.filter(state=state).values_list('polling_unit', flat=True))).sort()
-        return list(set(Location.objects.filter(ward=ward).values_list('polling_unit', flat=True))).sort()
+            return sorted(set(Location.objects.filter(state=state).values_list('polling_unit', flat=True)))
+        return sorted(set(Location.objects.filter(ward=ward).values_list('polling_unit', flat=True)))
