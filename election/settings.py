ce4063475ad6d245cbd824ddcf4c78174761cf0f
Ironmerc <onasanyatunde1234@gmail.com>
2023-01-16 22:24:40 +0100

    Set page size to 25

--- election/settings.py
+++ election/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
         'users.renderers.CustomRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8,
+    'PAGE_SIZE': 25,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 
 }
